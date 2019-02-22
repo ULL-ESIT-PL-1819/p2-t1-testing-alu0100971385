61a0fcc835ca8c14c57d65227802752361b0c48b
Daniel González <alu0100971385@ull.edu.es>
2019-02-22 14:35:55 +0000

    reto y ejercicios completados

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -10,14 +10,33 @@ module.exports = rdf => {
     
     book.title = $('dcterms\\:title').text();
     
-    book.authors = $('pgterms\\:agent pgterms\\:name')
-    .toArray().map(elem => $(elem).text());
+    /*book.authors = $('pgterms\\:agent pgterms\\:name')
+    .toArray().map(elem => $(elem).text());*/
+    
+    book.authors = $('pgterms\\:agent').toArray().map((elem) => {
+        let value = {};
+        value.name = $(elem).find("pgterms\\:name").text();
+        value.webpages = $(elem).find("pgterms\\:webpage").toArray().map((elem) => {
+            let value = $(elem).attr("rdf:resource");
+            return value;
+        });
+        
+        
+        return value;
+    });
     
     book.subjects = $('[rdf\\:resource$="/LCSH"]')
     .parent().find('rdf\\:value')
     .toArray().map(elem => $(elem).text());
     
     book.lcc = $('[rdf\\:resource$="/LCC"]').parent().find('rdf\\:value').text();
+    
+    book.sources =  $("pgterms\\:file").toArray().map((elem) => {
+        let value = {};
+        value.link = $(elem).attr("rdf:about"); //podemos acceder directamente
+        value.type = $(elem).find("rdf\\:Description").find("rdf\\:value").text(); //tenemos que hacer un find
+        return value;
+    });
     
     return book;
 };
--- databases/test/parse-rdf-test.js
+++ databases/test/parse-rdf-test.js
@@ -19,18 +19,27 @@ describe('parseRDF',() => {
         expect(book).to.have.a.property('title','The Art of War');
         
         expect(book).to.have.a.property('authors')
-        .that.is.an('array').with.lengthOf(2)
-        .and.contains('Sunzi, active 6th century B.C.')
-        .and.contains('Giles, Lionel');
+        .that.is.an('array').with.lengthOf(2);
         
         expect(book).to.have.a.property('subjects')
         .that.is.an('array').with.lengthOf(2)
         .and.contains('Military art and science -- Early works to 1800')
         .and.contains('War -- Early works to 1800');
         
+        
+    })
+    
+    it ('exercises from the book', () => {
+        
+        const book = parseRDF(rdf)
         expect(book).to.have.a.property('lcc')
         .that.is.a('string').with.lengthOf.at.least(1)
         .and.match(/^[^IOWXY]\w*$/);
-    })
+        
+        expect(book).to.have.a.property('sources')
+        .that.is.an('array').with.lengthOf.at.least(10);
+        
+        
+    });
 });
 
